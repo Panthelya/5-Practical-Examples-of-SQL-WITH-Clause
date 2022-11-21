d398d73b1f4f6034e46c8376dc7a1e76ddb66532
Panthelya <116551755+Panthelya@users.noreply.github.com>
2022-11-21 15:35:10 +0100

    Add files via upload

---
+++ revenue_DB.sql
@@ -0,0 +1,26 @@
+CREATE TABLE revenue (
+	id INT, 
+	year INT,
+	quarter VARCHAR (50),
+	revenue_amount DECIMAL (10,2)
+);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (1, 2019, 'Q1', 1589745.56);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (2, 2019, 'Q2', 2497845.41);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (3, 2019, 'Q3', 984157.15);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (4, 2019, 'Q4', 5417884.15);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (5, 2020, 'Q1', 2497441.68);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (6, 2020, 'Q2', 4448741.15);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (7, 2020, 'Q3', 9847415.14);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (8, 2020, 'Q4', 4125489.65);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (9, 2021, 'Q1', 12478945.47);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (10, 2021, 'Q2', 8459745.69);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (11, 2021, 'Q3', 4874874.51);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (12, 2021, 'Q4', 5123456.87);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (13, 2022, 'Q1', 4112587.26);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (14, 2022, 'Q2', 6459124.65);
+INSERT INTO revenue (id, year, quarter, revenue_amount) VALUES (15, 2022, 'Q3', 7894561.55);
+
+
+
+
+

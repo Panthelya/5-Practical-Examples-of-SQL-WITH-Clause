b9d4ef90bf28693427ee76d6321407d40a3a5aa0
Panthelya <116551755+Panthelya@users.noreply.github.com>
2022-11-21 10:05:39 +0100

    Add files via upload

---
+++ employees_DB.sql
@@ -0,0 +1,22 @@
+CREATE TABLE employees (
+	id INT, 
+	first_name VARCHAR (50),
+	last_name VARCHAR (50),
+	salary DECIMAL (10,2),
+	department VARCHAR (50)
+	
+);
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (1, 'Melissa', 'Allman', 5412.47, 'Accounting');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (2, 'Nina', 'Longhetti', 4125.79, 'Controlling');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (3, 'Steve', 'Clemence', 3157.99, 'Accounting');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (4, 'John', 'Sample', 5978.15, 'Controlling');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (5, 'Carole', 'Charles', 6897.47, 'Accounting');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (6, 'Etta', 'Cobham', 4579.55, 'Controlling');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (7, 'James', 'Johnson', 4455.66, 'Accounting');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (8, 'Victor', 'Hooper', 6487.47, 'Controlling');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (9, 'Bettye', 'James', 4597.88, 'Accounting');
+INSERT INTO employees (id, first_name, last_name, salary, department) VALUES (10, 'Susan', 'Trucks', 5497.45, 'Controlling');
+
+
+
+
